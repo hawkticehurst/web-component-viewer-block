806f7a5e84f4a91c6ea7a9a712f212d258b23acf
Hawk Ticehurst <hawkticehurst@gmail.com>
2023-01-24 15:53:13 -0800

    does it work now

--- blocks/example-file-block/index.svelte
+++ blocks/example-file-block/index.svelte
@@ -1,6 +1,13 @@
 <script lang="ts">
   import { FileBlockProps } from '@githubnext/blocks';
   export let { content } = $$props as FileBlockProps;
+  
+  try {
+    window.eval(content);
+  } catch(err) {
+    console.log("There was an error:");
+    console.log(err);
+  }
 </script>
 
-{@html content}
+<counter-button></counter-button>
